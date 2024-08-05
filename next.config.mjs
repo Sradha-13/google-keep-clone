7d0fe0a005d2b9d93ef081ec9ca132603faa6efd
Sushree Sradhanjali Satapathy <sushreesradha013@gmail.com>
2024-08-06 00:09:28 +0530

    updated config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+    reactStrictMode: true,
+    // Add more configurations if necessary
+  }
+  
 export default nextConfig;
